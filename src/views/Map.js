963180a75a7ce85f6b6d07c92ffd00eea6dd92a1
Tomas Korcak <korczis@gmail.com>
2015-08-01 14:09:08 +0200

    Simple map view

--- src/views/Map.js
+++ src/views/Map.js
@@ -5,10 +5,19 @@ const styles = {
         marginTop: '-20px',
         height: '600px',
         width: '100%'
+    },
+    stats: {
+        zIndex: 100
     }
 };
 
 class Map extends Component {
+    constructor(props) {
+        super(props);
+
+        this.layer = null;
+        this.stats = null;
+    }
     componentDidMount(rootNode) {
         var mapOptions = {
                 center: this.mapCenterLatLng(),
@@ -17,7 +26,23 @@ class Map extends Component {
 
         var map = new google.maps.Map(React.findDOMNode(this.refs.map), mapOptions);
         var marker = new google.maps.Marker({position: this.mapCenterLatLng(), title: 'Hi', map: map});
+
+        this.layer = new ThreejsLayer({map: map}, function (layer) {
+
+        });
+
         this.setState({map: map});
+
+        var stats = new Stats();
+        stats.setMode(0); // 0: fps, 1: ms
+
+
+        React.findDOMNode(this.refs.stats).appendChild(stats.domElement);
+
+        this.stats = stats;
+
+        // Start ticking
+        requestAnimationFrame(this.tick.bind(this));
     }
 
     mapCenterLatLng() {
@@ -27,8 +52,24 @@ class Map extends Component {
 
     render() {
         return (
-            <div ref='map' style={styles.map}></div>
+            <div>
+
+                <div ref='map' style={styles.map}></div>
+                <div ref="stats" style={styles.stats} />
+            </div>
         );
+    }
+
+    tick() {
+        // Begin of stats loop
+        this.stats.begin();
+
+        // TODO: Do heavy work here
+
+        // Begin of stats loop
+        this.stats.end();
+
+        requestAnimationFrame(this.tick.bind(this));
     }
 }
 
