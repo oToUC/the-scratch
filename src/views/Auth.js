e08713734e17d5892063359ff5e509b2245a3fe9
Tomas Korcak <korczis@gmail.com>
2015-08-11 15:15:51 +0200

    Simple auth view

--- src/views/Auth.js
+++ src/views/Auth.js
@@ -5,7 +5,17 @@ export default class Auth extends Component {
         return (
             <div>
                 <div className="container">
-                    <h1>Auth</h1>
+                    <div className="row text-center">
+                        <h1>
+                            Login or Sign-up
+                        </h1>
+
+                        <a href="/auth/facebook"><img src="/img/sn/facebook_128.png" id="authFacebookLink"/></a>
+                        <a href="/auth/github"><img src="/img/sn/github_128.png" id="authGithubLink"/></a>
+                        <a href="/auth/google"><img src="/img/sn/google_128.png" id="authGoogleLink" /></a>
+                        < a href="/auth/linkedin"><img src="/img/sn/linkedin_128.png" id="authLinkedinLink" /></a>
+                        <a href="/auth/twitter"><img src="/img/sn/twitter_128.png" id="authTwitterLink" /></a>
+                    </div>
                 </div>
             </div>
         );
