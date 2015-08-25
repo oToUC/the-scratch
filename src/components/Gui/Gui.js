adff05b26a1e437ad0432cacf18f54ed1805e7f9
Tomas Korcak <korczis@gmail.com>
2015-08-25 20:05:35 +0200

    Use scsss

--- src/components/Gui/Gui.js
+++ src/components/Gui/Gui.js
@@ -4,53 +4,12 @@ import Immutable from 'immutable';
 import GuiFolder from './Folder';
 
 export default class Gui extends Component {
-  static style = {
-    dragger: {
-      width: '6px',
-      marginRight: '-3px',
-      cursor: 'ew-resize',
-      position: 'absolute',
-      right: 0,
-      backgroundColor: 'red',
-      top: 0,
-      bottom: 0
-    },
-
-    gui: {
-      position: 'relative',
-      backgroundColor: 'black',
-      color: 'white',
-      width: '215px',
-      // resize: 'both',
-      overflowX: 'hidden',
-      overflowY: 'auto',
-      float: 'left',
-      clear: 'both',
-      paddingRight: '3px',
-      marginBottom: '10px'
-    },
-
-    li: {
-      paddingTop: 0,
-      paddingRight: '4px',
-      paddingBottom: 0,
-      paddingLeft: '5px',
-      overflow: 'hidden',
-      lineHeight: '27px',
-      height: '27px'
-    },
-
-    ul: {
-      margin: 0,
-      padding: 0,
-      clear: 'both',
-      listStyle: 'none',
-      width: '100%'
-    }
+  static defaultProps = {
+    style: Immutable.fromJS(require('./Gui.scss')).toJS()
   };
 
   state = {
-    style: Immutable.fromJS(Gui.style).toJS()
+    width: 200
   };
 
   constructor(props) {
@@ -89,20 +48,20 @@ export default class Gui extends Component {
     this.dragStartX = e.clientX;
 
     const state = this.state;
-    state.style.gui.width = (parseInt(state.style.gui.width.replace('px', '')) + diff) + "px";
+    state.width += diff;
     this.setState(state);
   }
 
   render() {
     return (
-      <div className="menu" style={this.state.style.gui} ref="gui">
+      <div className={this.props.style.gui + ' menu'} style={{width: this.state.width + 'px'}}ref="gui">
         {/* GUI List items */}
         <GuiFolder header={this.props.header}>
           {this.props.children}
         </GuiFolder>
 
         {/* Handle for dragging */}
-        <div style={this.state.style.dragger}
+        <div className={this.props.style.dragger}
              onMouseDown={this.onDragStart.bind(this)}
         ></div>
       </div>
--- src/components/Gui/Input.js
+++ src/components/Gui/Input.js
@@ -1,42 +1,12 @@
 import React, {Component, PropTypes} from 'react';
+import Immutable from 'immutable';
 
 export default class GuiInput extends Component {
-  static style = {
-    field: {
-      float: 'left',
-      width: '60%'
-    },
-
-    input: {
-      position: 'relative',
-      color: '#1ed36f',
-      width: '100%',
-      background: '#303030',
-      border: 0,
-      outline: 'none',
-      placeholder: 'white'
-    },
-
-    span: {
-      cursor: 'default',
-      float: 'left',
-      clear: 'left',
-      width: '40%',
-      fontFamily: "'Lucida Grande', sans-serif",
-      fontSize: '11px'
-    },
-
-    label: {
-      overflow: 'hidden',
-      textOverflow: 'ellipsis'
-    }
-  };
-
-  static listItemClassName = 'field input';  
+  static listItemClassName = 'field input';
 
   static defaultProps = {
     type: 'text',
-    style: GuiInput.style
+    style: Immutable.fromJS(require('./Input.scss')).toJS()
   };
 
   state = {
@@ -72,14 +42,14 @@ export default class GuiInput extends Component {
 
   render() {
     return (
-      <div>
-        <span style={GuiInput.style.span}>
-          <label style={GuiInput.style.label}>{this.props.title}</label>
+      <div className={this.props.style.guiInput}>
+        <span className={this.props.style.span}>
+          <label className={this.props.style.label}>{this.props.title}</label>
         </span>
-        <div style={GuiInput.style.field}>
+        <div className={this.props.style.field}>
           <input
             {...this.props}
-            style={GuiInput.style.input}
+            className={this.props.style.input}
           />
         </div>
       </div>
